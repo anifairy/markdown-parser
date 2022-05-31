af9c4eb0b16ffa8378720adf532a219c57906cad
anifairy <43860686+anifairy@users.noreply.github.com>
2022-05-31 00:15:39 -0700

    newfile

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -68,4 +68,22 @@ public class MarkdownParseTest {
         }
     
     }
+    @Test
+    public void myTestSnip1() throws IOException {
+        String contents = Files.readString(Path.of("test-snip-1.md"));
+        List<String> expect = List.of('`[a link](url.com)','another link`','cod[e', 'code]');
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void myTestSnip2() throws IOException {
+        String contents = Files.readString(Path.of("test-snip-2.md"));
+        List<String> expect = List.of('[a nested link](b.com)','a nested parenthesized url`','some escaped [ brackets ]s');
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }\
+    @Test
+    public void myTestSnip3() throws IOException {
+        String contents = Files.readString(Path.of("test-snip-3.md"));
+        List<String> expect = List.of('[this title text is really long and takes up more than one line and has some line breaks]( https://www.twitter.com )','this title text is really long and takes up more than one line',"[this link doesn't have a closing parenthesis](github.com And there's still some more text after that. [this link doesn't have a closing parenthesis for a while](https://cse.ucsd.edu/) And then there's more text");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
 }
